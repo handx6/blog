c0ea834f51763378ba8f056618e4aeabf814b10e
Martin Nicolas <mjld.nicolas@gmail.com>
2023-03-28 16:49:31 +0200

    end of day

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,22 +2,58 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Post;
+use App\Form\PostFormType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Entity;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
     #[Route('/', name:'app_home')]
-    public function index(): Response
+    public function index(PostRepository $repo): Response
     {
-        return $this->render('blog/index.html.twig');
+        // 1 - récupère tous les posts
+        $posts = $repo->findAll();
+        // 2 - Envoie la data à la vue
+        return $this->render('blog/index.html.twig', compact('posts'));
     }
 
-    #[Route('/about')]
-    public function aboutUs(): Response
+    #[Route('/post/{id}', name: 'app_show')]
+    public function show($id, PostRepository $repo): Response
     {
-        return $this->render('blog/about.html.twig');
+        // Je récupère le post avec l'id
+        $post = $repo->find($id);
+        return $this->render('blog/show.html.twig', compact('post'));
     }
 
+    #[Route('/post/delete/{id}', name: 'app_delete', methods: ['GET', 'DELETE'])]
+    public function delete($id, PostRepository $repo, EntityManagerInterface $em): Response
+    {
+        // 1 - Je récupère le post avec l'id
+        $post = $repo->find($id);
+        // 2 - Je supprime le post
+        $em->remove($post);
+        // 3 - Tire la chasse
+        $em->flush();
+        // 4 - Redirection vers la page d'accueil
+        return $this->redirectToRoute('app_home');
+    }
+
+    #[Route('/create', name: 'app_create')]
+    public function create(PostRepository $repo): Response
+    {
+        // 1 - Create new object
+        $post = new Post();
+        // 2 - create form 
+        $form = $this->createForm(PostFormType::class, $post);
+        $showForm = $form->createView();
+
+        // 3 - send form to view
+        return $this->render('blog/create.html.twig', compact('showForm'));
+    }
 }
---
+++ src/Form/PostFormType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class PostFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'form',
+                    'placeholder' => "Titre de l'article"
+                ]
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => [
+                    'class' => 'form',
+                ]
+            ])
+            ->add('url_img', TextType::class, [
+                'attr' => [
+                    'class' => 'form',
+                ]
+            ])
+            ->add('author', TextType::class, [
+                'attr' => [
+                    'class' => 'form',
+                ]
+            ])
+            ->add('category', TextType::class, [
+                'attr' => [
+                    'class' => 'form',
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
