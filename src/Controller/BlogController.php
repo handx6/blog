20cf2f0ff375e09b5117090033fffdcc0c32c7f6
Martin Nicolas <mjld.nicolas@gmail.com>
2023-03-28 13:55:21 +0200

    new webapp blog

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/', name:'app_home')]
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig');
+    }
+
+    #[Route('/about')]
+    public function aboutUs(): Response
+    {
+        return $this->render('blog/about.html.twig');
+    }
+
+}
